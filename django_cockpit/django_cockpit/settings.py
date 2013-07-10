7709ce1abd7efd3f84590ca39f1088b26208813b
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-11 01:35:23 +0300

    Added django-mptt to the example project.

--- django_cockpit/django_cockpit/settings.py
+++ django_cockpit/django_cockpit/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'ckeditor',
+    'mptt',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'cockpit',
