f8b615989e6f3b010dfded1036107612db8ea13a
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-11 01:55:32 +0300

    ckeditor correction. Basic model test for page.

---
+++ cockpit/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from cockpit.models import Page
+
+admin.site.register(Page)
--- cockpit/models.py
+++ cockpit/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Page(models.Model):
+    header = models.CharField(max_length=200)
+    content = RichTextField()
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.header
