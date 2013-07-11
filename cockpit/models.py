468217ad8e9356eba5bf6f568515dd6d488cce95
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-11 14:06:05 +0300

    Added "requirements.txt". Added mptt to page model.

--- cockpit/models.py
+++ cockpit/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from ckeditor.fields import RichTextField
 
-class Page(models.Model):
-    header = models.CharField(max_length=200)
+
+class Page(MPTTModel):
+    header = models.CharField(max_length=200, unique=True)
     content = RichTextField()
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['header']
+
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.header
