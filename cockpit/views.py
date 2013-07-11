6957dec88ae4aa10f4e8b4197a1242179b1ea5f2
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-11 14:51:50 +0300

    Basic views, templates.

--- cockpit/views.py
+++ cockpit/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from cockpit.models import Page
+
+
+def index(request):
+    latest_pages = Page.objects.order_by('id')[:10]
+    context = {'latest_pages': latest_pages}
+    return render(request, 'cockpit/index.html', context)
+
+
+def detail(request, page_id):
+    page = get_object_or_404(Page, pk=page_id)
+    return render(request, 'cockpit/page.html', {'page': page})
--- django_cockpit/django_cockpit/urls.py
+++ django_cockpit/django_cockpit/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_cockpit.views.home', name='home'),
     # url(r'^django_cockpit/', include('django_cockpit.foo.urls')),
 
+    url(r'^cockpit/', include('cockpit.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
