b7129afef8271e6d82bd2a898d6a1845165d2234
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-13 19:49:43 +0300

    Broken.

--- cockpit/models.py
+++ cockpit/models.py
@@ -4,9 +4,10 @@ from django.utils.translation import ugettext_lazy as _
 from hvad.models import TranslatableModel, TranslatedFields
 from ckeditor.fields import RichTextField
 from noconflict import classmaker
+import mptt
 
 
-class Page(TranslatableModel, MPTTModel):
+class Page(TranslatableModel):
     """
     Pages with hierarchical structure.
     """
@@ -14,7 +15,7 @@ class Page(TranslatableModel, MPTTModel):
     # Prevent metaclass conflict
     # https://github.com/ojii/django-nani/issues/39
     # http://code.activestate.com/recipes/204197-solving-the-metaclass-conflict/
-    __metaclass__=classmaker()
+    #__metaclass__=classmaker()
 
     translations = TranslatedFields(
         heading=models.CharField(_('heading'), max_length=200, unique=True),
@@ -22,11 +23,11 @@ class Page(TranslatableModel, MPTTModel):
         slug=models.SlugField(_('slug'), unique=True,)
     )
 
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
-
-    class MPTTMeta:
-        order_insertion_by = ['heading']
 
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.heading
+
+TreeForeignKey(Page, blank=True, null=True).contribute_to_class(Page, 'parent')
+
+mptt.register(Page)
--- cockpit/views.py
+++ cockpit/views.py
@@ -4,14 +4,14 @@ from cockpit.models import Page
 
 
 def index(request):
-    pages = Page.objects.all()
+    pages = Page.objects.language('bg').all()
     context = {'pages': pages}
     return render(request, 'cockpit/index.html', context)
 
 
 def detail(request, slug):
     try:
-        page = Page.objects.language().get(slug=slug)
+        page = Page.objects.language('bg').get(slug=slug)
     except Page.DoesNotExist:
         raise Http404
     return render(request, 'cockpit/page.html', {'page': page})
