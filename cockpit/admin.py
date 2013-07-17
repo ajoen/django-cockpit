b3e8cd91a5b3f12910c673decc4d0adf20e02679
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-17 16:07:16 +0300

    Drag-drop hierarchy editing is completed.

--- cockpit/admin.py
+++ cockpit/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from hvad.admin import TranslatableAdmin
 from cockpit.models import Page
+from django.conf.urls import patterns, url
+from django.http import HttpResponseRedirect
 
 
 class PageAdmin(TranslatableAdmin):
@@ -20,5 +22,39 @@ class PageAdmin(TranslatableAdmin):
     search_fields = ['translations__heading']
     date_hierarchy = 'created_at'
 
+    def get_urls(self):
+        """
+        Overrided to include a custom admin page "change_parent".
+        """
+        urls = super(PageAdmin, self).get_urls()
+        my_urls = patterns('',
+            url(r'^change_parent/$', self.admin_site.admin_view(self.change_parent), name='change_parent')
+        )
+        return my_urls + urls
+
+    def change_parent(self, request):
+        """
+        This view receives "next", "change_parent_of" and "change_parent_to" parameters
+        and changes page hierarchy accordingly. Redirects to the "next" parameter
+        afterwards.
+        """
+        redirect_to = request.GET['next']
+        change_parent_of = Page.objects.get(pk=request.GET['change_parent_of'])
+        change_parent_to = Page.objects.get(pk=request.GET['change_parent_to'])
+        change_parent_of.parent_id = change_parent_to.id
+
+        # Check if there is cycle in the tree.
+        parent_cycle = change_parent_to
+        while True:
+            if parent_cycle.parent_id is None:
+                break
+            if parent_cycle.parent_id == change_parent_of.id:
+                parent_cycle.parent_id = None
+                break
+            parent_cycle = Page.objects.get(pk=parent_cycle.parent_id)
+
+        change_parent_of.save()
+        parent_cycle.save()
+        return HttpResponseRedirect(redirect_to)
 
 admin.site.register(Page, PageAdmin)
